d5001dbb99a0c86352dba2d59a042a7efe9297ff
Ben Ackroyd <ben@ackro.net>
2018-03-02 11:58:02 +1100

    Update base installation tasks

--- templates/get_stage.sh
+++ templates/get_stage.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-latest="http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3.txt"
-stage3_path=$(curl  $latest 2>&1 \
-            | awk '/\/stage3-amd64-[0-9]+.tar.bz2/{print $1}')
+install_path="/mnt/gentoo"
+latest="latest-stage3-amd64.txt"
+mirror="{{ mirror }}"
+latest_uri="{{ mirror }}/releases/amd64/autobuilds/$latest"
 
-wget  {{ mirror }}/releases/amd64/autobuilds/$stage3_path -O \
-      stage3.tbz2
+if [ ! -f "$install_path/$latest_uri" ]
+then
+  curl -sO $latest_uri
+fi
+
+if [ -z "$SHOW_RELEASE_NAME" ]
+then
+  stage3_path=$(cat $install_path/$latest | awk '/stage3-amd64-[0-9TZ]+\.tar\.xz/ { print $1 }')
+  curl -sO  "$mirror/releases/amd64/autobuilds/$stage3_path"
+  curl -sO  "$mirror/releases/amd64/autobuilds/$stage3_path.DIGESTS.asc"
+  awk 'f{print $1, $2;f=0;exit}/SHA512/{f=1}' *.DIGESTS.asc > stage3.sha512
+else
+  cat $install_path/$latest | awk -F'/' '/stage3-amd64-[0-9TZ]+\.tar\.xz/ { print $1 }'
+fi
